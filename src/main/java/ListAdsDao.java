39f79eefb078e6379e6355c6a87c385da745df5f
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-08 16:04:16 -0600

    add work for ListAdsDao

--- src/main/java/ListAdsDao.java
+++ src/main/java/ListAdsDao.java
@@ -1,3 +1,42 @@
-public class ListAdsDao{
+import java.util.ArrayList;
+import java.util.List;
 
+public class ListAdsDao implements Ads {
+    private List<Ad> ads;
+
+    public List<Ad> all() {
+        if (ads == null) {
+            ads = generateAds();
+        }
+        return ads;
+    }
+
+    private List<Ad> generateAds() {
+        List<Ad> ads = new ArrayList<>();
+        ads.add(new Ad(
+                1,
+                1,
+                "Nintendo Switch for sale",
+                "Used a lot in really bad condition, asking for retail price"
+        ));
+        ads.add(new Ad(
+                2,
+                1,
+                "Nintendo 64",
+                "I just want to get rid of this"
+        ));
+        ads.add(new Ad(
+                3,
+                2,
+                "Mouse",
+                "Im not sure which mouse this is"
+        ));
+        ads.add(new Ad(
+                4,
+                2,
+                "Monitor",
+                "32in 144hz va panel"
+        ));
+        return ads;
+    }
 }

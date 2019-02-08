0e363bd2dda035fc0c3650518a85591e120e8c08
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-08 15:52:26 -0600

    add ListsAdsDao and work for DaoFactory'

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,2 +1,10 @@
 public class DaoFactory {
+    private static Ads adsDao;
+
+    public static Ads getAdsDao() {
+        if (adsDao == null) {
+            adsDao = new ListAdsDao();
+        }
+        return adsDao;
+    }
 }
